a9c0761cb67a276e08ae0749b13f0fd1bc1b1a5f
Himorell <himorellj@gmail.com>
2022-12-23 00:32:33 +0100

    Add images logo and fixed json

--- js/index.js
+++ js/index.js
@@ -1,37 +1,21 @@
 const requestURL = "../json/characters.json";
 
-//Llamada asíncrona: "espera que llegue todo antes de seguir"
+
 async function fetchAllCharactersJson(){
     const response = await fetch(requestURL);
     const allCharacters = await response.json();
     return allCharacters;
 }
-//Si todo es correcto, vas a leer "allCharacters"
+
 fetchAllCharactersJson().then(allCharacters =>{
 
     for (let index = 0; index < allCharacters.characters.length; index++){
 
         const charactersSection = document.getElementById('charactersSection');
 
-        /* let houseStark = allCharacters.characters[index].houseName['stark'];
-        let houseTargaryen = allCharacters.characters[index].houseName['Targaryen'];
-        let houseGreyjoy = allCharacters.characters[index].houseName['Greyjoy'];
-        let houseFrey = allCharacters.characters[index].houseName['Frey']; */
-        
-
         let characterName = allCharacters.characters[index].characterName;
         let houseName = allCharacters.characters[index].houseName;
-        /* let houseName = allCharacters.characters[index].houseName['stark'];  */
         let characterImageFull = allCharacters.characters[index].characterImageFull;
-
-
-
-        /* document.getElementById('North').onclick=function(){
-            // Remove any element-specific value, falling back to stylesheets
-            document.getElementById('stark').style.display='';
-          }; */
-
-    
 
         charactersSection.innerHTML += `
             <div class="card ${houseName}" style="width: 6rem;">
@@ -41,19 +25,5 @@ fetchAllCharactersJson().then(allCharacters =>{
                 </div>
             </div>
         `
-        //console.log(allCharacters.characters[index].characters)
     };
 })
-
-
-
-/* const targetDiv = document.getElementsByClassName("Greyjoy");
-const btn = document.getElementsByClassName("Stark");
-btn.onclick = function () {
-    if (targetDiv.style.display !== "none"){
-    targetDiv.style.display = "none";
-    }
-    if (targetDiv.style.display !== "none"){
-    targetDiv.style.display = "block";
-    }
-}; */
